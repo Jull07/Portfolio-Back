bf144b92687bc5b0d534737426e9895ac4549ad8
Jull07 <Jull07>
2023-04-10 06:36:11 -0300

    Volviendo atras 2

--- src/main/java/com/portfolio/julcuevas/Security/mainSecurity.java
+++ src/main/java/com/portfolio/julcuevas/Security/mainSecurity.java
@@ -15,7 +15,6 @@ import org.springframework.security.config.annotation.authentication.configurati
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -62,8 +61,5 @@ public class mainSecurity {
 
         return http.build();
     }
-      @Bean
-    public WebSecurityCustomizer webSecurityCustomizer() {
-        return (web) -> web.ignoring().requestMatchers("/auth", "/login", "/auth/login");
-    }
+    
 }

270ff961afda437ab3a1bb19566ca54bc06cd1a3
Jull07 <Jull07>
2023-04-11 03:44:18 -0300

    Revert "Borrando corchetes"
    This reverts commit 43909ea4d2a951118a17d140ef1e7dac82004728.

--- src/main/java/com/portfolio/julcuevas/Security/Controller/AuthController.java
+++ src/main/java/com/portfolio/julcuevas/Security/Controller/AuthController.java
@@ -31,7 +31,7 @@ import java.util.Set;
 
 @RestController
 @RequestMapping("/auth")
-@CrossOrigin(origins= "https://frontend-6f20b.web.app")
+@CrossOrigin(origins= {"https://frontend-6f20b.web.app"})
 public class AuthController {
 
     @Autowired

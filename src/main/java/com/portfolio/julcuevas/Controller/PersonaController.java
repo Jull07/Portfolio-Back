615df3bc00188fbda441b9dffb13e590a2c4844b
Jull07 <Jull07>
2023-04-11 09:11:39 -0300

    Agregando cosas

--- src/main/java/com/portfolio/julcuevas/Controller/PersonaController.java
+++ src/main/java/com/portfolio/julcuevas/Controller/PersonaController.java
@@ -14,11 +14,13 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 
 @RestController
+@RequestMapping("/personas")
 @CrossOrigin(origins = "https://frontend-6f20b.web.app")
 public class PersonaController {
       @Autowired 

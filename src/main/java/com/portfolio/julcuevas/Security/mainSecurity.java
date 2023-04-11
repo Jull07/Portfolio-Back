febcd4e8f48fb502bf5c70cc5b439c65eacac8b8
Jull07 <Jull07>
2023-04-11 03:44:25 -0300

    Revert "Matchers 2"
    This reverts commit f1cf2bd2d796626f7010aa8fc66c8968219fea44.

--- src/main/java/com/portfolio/julcuevas/Security/mainSecurity.java
+++ src/main/java/com/portfolio/julcuevas/Security/mainSecurity.java
@@ -15,7 +15,6 @@ import org.springframework.security.config.annotation.authentication.configurati
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -61,9 +60,5 @@ public class mainSecurity {
         http.addFilterBefore(jwtTokenFilter(), UsernamePasswordAuthenticationFilter.class);
 
         return http.build();
-    }
-      @Bean
-    public WebSecurityCustomizer webSecurityCustomizer() {
-        return (web) -> web.ignoring().requestMatchers("/auth", "/login", "/auth/login");
     }
 }

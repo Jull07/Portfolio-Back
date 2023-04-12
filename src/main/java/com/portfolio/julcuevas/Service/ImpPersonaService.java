1a5f69f55ef7461c91f22ed66745cfd68f0049c3
Jull07 <Jull07>
2023-04-12 09:21:30 -0300

    Changes

--- src/main/java/com/portfolio/julcuevas/Service/ImpPersonaService.java
+++ src/main/java/com/portfolio/julcuevas/Service/ImpPersonaService.java
@@ -2,40 +2,49 @@
 package com.portfolio.julcuevas.Service;
 
 import com.portfolio.julcuevas.Entity.Persona;
-import com.portfolio.julcuevas.Interface.IPersonaService;
 import com.portfolio.julcuevas.Repository.IPersonaRepository;
 import java.util.List;
+import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 
 
 
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
+@Transactional
+public class ImpPersonaService {
 
-public class ImpPersonaService implements IPersonaService{
-    @Autowired 
-            private IPersonaRepository ipersonaRepository;
+    @Autowired
+    IPersonaRepository ipersonaRepository;
     
-    @Override
-    public List<Persona> getPersona() {
-        List<Persona> persona = ipersonaRepository.findAll();
-                return persona;
-    }
-
-    @Override
-    public void savePersona(Persona persona) {
-        ipersonaRepository.save(persona);
-    }
-
-    @Override
-    public void deletePersona(Long Id) {
-         ipersonaRepository.deleteById(Id);    }
-
-    @Override
-    public Persona findPersona(Long Id) {
-         Persona persona = ipersonaRepository.findById(Id).orElse(null);
-    return persona; }
-    
+    public List<Persona> list(){
+         return ipersonaRepository.findAll();
+     }
+     
+     public Optional<Persona> getOne(int id){
+         return ipersonaRepository.findById(id);
+     }
+     
+     public Optional<Persona> getByNombre(String nombre){
+         return ipersonaRepository.findByNombre(nombre);
+     }
+     
+     public void save(Persona persona){
+         ipersonaRepository.save(persona);
+     }
+     
+     public void delete(int id){
+         ipersonaRepository.deleteById(id);
+     }
+     
+     public boolean existsById(int id){
+         return ipersonaRepository.existsById(id);
+     }
+     
+     public boolean existsByNombre(String nombre){
+         return ipersonaRepository.existsByNombre(nombre);
+     }
 }
-    
+

6513ae4ca2132d886b509fbdbd0153ab391b9209
Jull07 <Jull07>
2023-04-12 00:14:15 -0300

    Agregando permitidos

--- src/main/java/com/portfolio/julcuevas/JulcuevasApplication.java
+++ src/main/java/com/portfolio/julcuevas/JulcuevasApplication.java
@@ -2,6 +2,9 @@ package com.portfolio.julcuevas;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class JulcuevasApplication {
@@ -9,5 +12,18 @@ public class JulcuevasApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(JulcuevasApplication.class, args);
 	}
+        @Bean
+public WebMvcConfigurer corsConfigurer() {
+  return new WebMvcConfigurer() {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+      registry.addMapping("/**")
+         /* en allowedOrigins va la URL del backend y de localhost para poder hacer pruebas*/
+        .allowedOrigins("https://backend-bl22.onrender.com", "ht tp://localhost:4200/")
+        /* Aqui va los métodos permitidos, puse asterisco para que se permitan todos porque por defecto solo permite get*/
+        .allowedMethods("*");
+    }
+  };
+}  
 
 }

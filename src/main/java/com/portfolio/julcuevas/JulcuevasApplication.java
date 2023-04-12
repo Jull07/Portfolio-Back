447e66eed22b390654192c819a4c89a0b81c165f
Jull07 <Jull07>
2023-04-12 00:32:33 -0300

    cors

--- src/main/java/com/portfolio/julcuevas/JulcuevasApplication.java
+++ src/main/java/com/portfolio/julcuevas/JulcuevasApplication.java
@@ -2,9 +2,6 @@ package com.portfolio.julcuevas;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class JulcuevasApplication {
@@ -12,18 +9,6 @@ public class JulcuevasApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(JulcuevasApplication.class, args);
 	}
-        @Bean
-public WebMvcConfigurer corsConfigurer() {
-  return new WebMvcConfigurer() {
-    @Override
-    public void addCorsMappings(CorsRegistry registry) {
-      registry.addMapping("/**")
-         /* en allowedOrigins va la URL del backend y de localhost para poder hacer pruebas*/
-        .allowedOrigins("https://backend-bl22.onrender.com", "http://localhost:4200/")
-        /* Aqui va los métodos permitidos, puse asterisco para que se permitan todos porque por defecto solo permite get*/
-        .allowedMethods("*");
-    }
-  };
-}  
+        
 
 }
--- src/main/java/com/portfolio/julcuevas/WebConfig.java
+++ src/main/java/com/portfolio/julcuevas/WebConfig.java
@@ -18,7 +18,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry){
-        registry.addMapping("/''").allowedOrigins("https://frontend-6f20b.web.app", "http://localhost:4200/").allowedMethods("*");
+        registry.addMapping("/''").allowedOrigins("https://backend-bl22.onrender.com", "http://localhost:4200/").allowedMethods("*");
     }
     
 }

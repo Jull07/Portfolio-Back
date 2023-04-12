c3bc98a904b9993d26b1ecd966cf48f682baaea2
Jull07 <Jull07>
2023-04-12 02:39:54 -0300

    personacontroller

--- src/main/java/com/portfolio/julcuevas/Controller/PersonaController.java
+++ src/main/java/com/portfolio/julcuevas/Controller/PersonaController.java
@@ -25,25 +25,25 @@ import org.springframework.web.bind.annotation.RestController;
 public class PersonaController {
       @Autowired 
            private IPersonaService ipersonaService;
-      @GetMapping("/personas/traer")
+      @GetMapping("personas/traer")
   public List<Persona> getPersona() {
       return ipersonaService.getPersona();
   }
-     
+      @PreAuthorize("hasRole('ADMIN')")
       @PostMapping("/personas/crear")
       public String createPersona(@RequestBody Persona persona) {
           ipersonaService.savePersona(persona);
           return "La persona se creo correctramente";
       }
       
-     
+      @PreAuthorize("hasRole('ADMIN')")
       @DeleteMapping("/personas/borrar/{id}")
       public String deletePersona(@PathVariable Long id){
           ipersonaService.deletePersona(id);
           return "La persona se elimino correctamente";
       }
       
-    
+      @PreAuthorize("hasRole('ADMIN')")
       @PutMapping("/personas/editar/{id}")
       public Persona editPersona(@PathVariable Long id, @RequestParam("nombre") String nuevoNombre,
               @RequestParam("apellido") String nuevoApellido,
@@ -59,6 +59,6 @@ public class PersonaController {
     }
       @GetMapping("/personas/traer/perfil")
       public Persona findPersona() {
-          return ipersonaService.findPersona((long)1);
+          return ipersonaService.findPersona((long)5);
       }
 }

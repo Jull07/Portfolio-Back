aad21b2350d4ca0e389a70b3205bb461d42e7df9
Jull07 <Jull07>
2023-04-12 00:03:28 -0300

    Cors

--- src/main/java/com/portfolio/julcuevas/WebConfig.java
+++ src/main/java/com/portfolio/julcuevas/WebConfig.java
@@ -18,7 +18,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry){
-        registry.addMapping("/''");
+        registry.addMapping("/''").allowedOrigins("https://frontend-6f20b.web.app");
     }
     
 }

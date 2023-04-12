49ed071300f02a6af69d5a285f2c2ca7f9705d21
Jull07 <Jull07>
2023-04-12 04:11:29 -0300

    borrando personas/traer/perfil

--- src/main/java/com/portfolio/julcuevas/Controller/PersonaController.java
+++ src/main/java/com/portfolio/julcuevas/Controller/PersonaController.java
@@ -56,8 +56,5 @@ public class PersonaController {
           ipersonaService.savePersona(persona);
           return persona;
     }
-      @GetMapping("/personas/traer/perfil")
-      public Persona findPersona() {
-          return ipersonaService.findPersona((long)5);
-      }
+    
 }
